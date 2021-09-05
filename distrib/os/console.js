f479c5b349b65f59efbd8dec6b71a93df6a978ed
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-09-05 16:22:45 -0400

    Adding scrolling implementation using getImageData and putImageData - Closes #5

--- distrib/os/console.js
+++ distrib/os/console.js
@@ -26,11 +26,13 @@ var TSOS;
             this.currentYPosition = this.currentFontSize;
         }
         handleInput() {
+            console.log(_KernelInputQueue.toString());
             while (_KernelInputQueue.getSize() > 0) {
                 // Get the next character from the kernel input queue.
                 var chr = _KernelInputQueue.dequeue();
                 // Check to see if it's "special" (enter or ctrl-c) or "normal" (anything else that the keyboard device driver gave us).
-                if (chr === String.fromCharCode(13)) { // the Enter key
+                if (chr === String.fromCharCode(13)) {
+                    // the Enter key
                     // The enter key marks the end of a console command, so ...
                     // ... tell the shell ...
                     _OsShell.handleInput(this.buffer);
@@ -49,12 +51,12 @@ var TSOS;
         }
         putText(text) {
             /*  My first inclination here was to write two functions: putChar() and putString().
-                Then I remembered that JavaScript is (sadly) untyped and it won't differentiate
-                between the two. (Although TypeScript would. But we're compiling to JavaScipt anyway.)
-                So rather than be like PHP and write two (or more) functions that
-                do the same thing, thereby encouraging confusion and decreasing readability, I
-                decided to write one function and use the term "text" to connote string or char.
-            */
+                      Then I remembered that JavaScript is (sadly) untyped and it won't differentiate
+                      between the two. (Although TypeScript would. But we're compiling to JavaScipt anyway.)
+                      So rather than be like PHP and write two (or more) functions that
+                      do the same thing, thereby encouraging confusion and decreasing readability, I
+                      decided to write one function and use the term "text" to connote string or char.
+                  */
             if (text !== "") {
                 // Draw the text at the current X and Y coordinates.
                 _DrawingContext.drawText(this.currentFont, this.currentFontSize, this.currentXPosition, this.currentYPosition, text);
@@ -70,10 +72,22 @@ var TSOS;
              * Font descent measures from the baseline to the lowest point in the font.
              * Font height margin is extra spacing between the lines.
              */
-            this.currentYPosition += _DefaultFontSize +
+            const { width, height } = _Canvas;
+            const line = _DefaultFontSize +
                 _DrawingContext.fontDescent(this.currentFont, this.currentFontSize) +
                 _FontHeightMargin;
-            // TODO: Handle scrolling. (iProject 1)
+            const y = this.currentYPosition;
+            /*
+             * if we exceed the height then we do not want our y value to change we want to
+             * simply lower the contents of the canvas, we do this by replacing image data
+             */
+            if (y + line > height) {
+                const data = _DrawingContext.getImageData(0, line, width, height);
+                _DrawingContext.putImageData(data, 0, 0);
+            }
+            else {
+                this.currentYPosition += line; // advance the line if it doesnt exceed
+            }
         }
     }
     TSOS.Console = Console;
--- source/os/console.ts
+++ source/os/console.ts
@@ -6,81 +6,106 @@
      ------------ */
 
 module TSOS {
+  export class Console {
+    constructor(
+      public currentFont = _DefaultFontFamily,
+      public currentFontSize = _DefaultFontSize,
+      public currentXPosition = 0,
+      public currentYPosition = _DefaultFontSize,
+      public buffer = ""
+    ) {}
 
-    export class Console {
+    public init(): void {
+      this.clearScreen();
+      this.resetXY();
+    }
 
-        constructor(public currentFont = _DefaultFontFamily,
-                    public currentFontSize = _DefaultFontSize,
-                    public currentXPosition = 0,
-                    public currentYPosition = _DefaultFontSize,
-                    public buffer = "") {
+    public clearScreen(): void {
+      _DrawingContext.clearRect(0, 0, _Canvas.width, _Canvas.height);
+    }
+
+    public resetXY(): void {
+      this.currentXPosition = 0;
+      this.currentYPosition = this.currentFontSize;
+    }
+
+    public handleInput(): void {
+      console.log(_KernelInputQueue.toString());
+      while (_KernelInputQueue.getSize() > 0) {
+        // Get the next character from the kernel input queue.
+        var chr = _KernelInputQueue.dequeue();
+        // Check to see if it's "special" (enter or ctrl-c) or "normal" (anything else that the keyboard device driver gave us).
+        if (chr === String.fromCharCode(13)) {
+          // the Enter key
+          // The enter key marks the end of a console command, so ...
+          // ... tell the shell ...
+          _OsShell.handleInput(this.buffer);
+          // ... and reset our buffer.
+          this.buffer = "";
+        } else {
+          // This is a "normal" character, so ...
+          // ... draw it on the screen...
+          this.putText(chr);
+          // ... and add it to our buffer.
+          this.buffer += chr;
         }
+        // TODO: Add a case for Ctrl-C that would allow the user to break the current program.
+      }
+    }
 
-        public init(): void {
-            this.clearScreen();
-            this.resetXY();
-        }
-
-        public clearScreen(): void {
-            _DrawingContext.clearRect(0, 0, _Canvas.width, _Canvas.height);
-        }
-
-        public resetXY(): void {
-            this.currentXPosition = 0;
-            this.currentYPosition = this.currentFontSize;
-        }
-
-        public handleInput(): void {
-            while (_KernelInputQueue.getSize() > 0) {
-                // Get the next character from the kernel input queue.
-                var chr = _KernelInputQueue.dequeue();
-                // Check to see if it's "special" (enter or ctrl-c) or "normal" (anything else that the keyboard device driver gave us).
-                if (chr === String.fromCharCode(13)) { // the Enter key
-                    // The enter key marks the end of a console command, so ...
-                    // ... tell the shell ...
-                    _OsShell.handleInput(this.buffer);
-                    // ... and reset our buffer.
-                    this.buffer = "";
-                } else {
-                    // This is a "normal" character, so ...
-                    // ... draw it on the screen...
-                    this.putText(chr);
-                    // ... and add it to our buffer.
-                    this.buffer += chr;
-                }
-                // TODO: Add a case for Ctrl-C that would allow the user to break the current program.
-            }
-        }
-
-        public putText(text): void {
-            /*  My first inclination here was to write two functions: putChar() and putString().
+    public putText(text): void {
+      /*  My first inclination here was to write two functions: putChar() and putString().
                 Then I remembered that JavaScript is (sadly) untyped and it won't differentiate
                 between the two. (Although TypeScript would. But we're compiling to JavaScipt anyway.)
                 So rather than be like PHP and write two (or more) functions that
                 do the same thing, thereby encouraging confusion and decreasing readability, I
                 decided to write one function and use the term "text" to connote string or char.
             */
-            if (text !== "") {
-                // Draw the text at the current X and Y coordinates.
-                _DrawingContext.drawText(this.currentFont, this.currentFontSize, this.currentXPosition, this.currentYPosition, text);
-                // Move the current X position.
-                var offset = _DrawingContext.measureText(this.currentFont, this.currentFontSize, text);
-                this.currentXPosition = this.currentXPosition + offset;
-            }
-         }
-
-        public advanceLine(): void {
-            this.currentXPosition = 0;
-            /*
-             * Font size measures from the baseline to the highest point in the font.
-             * Font descent measures from the baseline to the lowest point in the font.
-             * Font height margin is extra spacing between the lines.
-             */
-            this.currentYPosition += _DefaultFontSize + 
-                                     _DrawingContext.fontDescent(this.currentFont, this.currentFontSize) +
-                                     _FontHeightMargin;
-
-            // TODO: Handle scrolling. (iProject 1)
-        }
+      if (text !== "") {
+        // Draw the text at the current X and Y coordinates.
+        _DrawingContext.drawText(
+          this.currentFont,
+          this.currentFontSize,
+          this.currentXPosition,
+          this.currentYPosition,
+          text
+        );
+        // Move the current X position.
+        var offset = _DrawingContext.measureText(
+          this.currentFont,
+          this.currentFontSize,
+          text
+        );
+        this.currentXPosition = this.currentXPosition + offset;
+      }
     }
- }
+
+    public advanceLine(): void {
+      this.currentXPosition = 0;
+      /*
+       * Font size measures from the baseline to the highest point in the font.
+       * Font descent measures from the baseline to the lowest point in the font.
+       * Font height margin is extra spacing between the lines.
+       */
+      const { width, height } = _Canvas;
+
+      const line =
+        _DefaultFontSize +
+        _DrawingContext.fontDescent(this.currentFont, this.currentFontSize) +
+        _FontHeightMargin;
+
+      const y = this.currentYPosition;
+
+      /*
+       * if we exceed the height then we do not want our y value to change we want to
+       * simply lower the contents of the canvas, we do this by replacing image data
+       */
+      if (y + line > height) {
+        const data = _DrawingContext.getImageData(0, line, width, height);
+        _DrawingContext.putImageData(data, 0, 0);
+      } else {
+        this.currentYPosition += line; // advance the line if it doesnt exceed
+      }
+    }
+  }
+}
