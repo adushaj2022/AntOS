af1e58572720e28527d2abedfff617437112d1d6
adushaj2022 <anthony.dushaj1@marist.edu>
2021-09-25 15:09:55 -0400

    Initial setup for memory.ts in host

---
+++ distrib/host/memory.js
@@ -0,0 +1,69 @@
+var TSOS;
+(function (TSOS) {
+    class Memory {
+        constructor() {
+            this.mainMemory = new Array(0xffff); //representation of memory
+            this.mar = 0x0000; //Memory Address Reg
+            this.mdr = 0x00; //Memory Data Reg
+            this.fillArray(); //when Memory is created, it will be initiated to 0
+        }
+        //setter for memory address register
+        setMAR(mar) {
+            this.mar = mar;
+        }
+        //getter for memory address register
+        getMAR() {
+            return this.mar;
+        }
+        //setter for memory data register
+        setMDR(mdr) {
+            this.mdr = mdr;
+        }
+        //getter for memory data register
+        getMDR() {
+            return this.mdr;
+        }
+        //sets mar, mdr, and all of memory to 0
+        reset() {
+            this.fillArray();
+            this.setMAR(0x0000);
+            this.setMDR(0x00);
+        }
+        //returns the data (mdr) from a specificed address (mar)
+        read() {
+            return this.mainMemory[this.getMAR()];
+        }
+        //sets the data (mdr) from a specificed address (mar)
+        write() {
+            this.mainMemory[this.getMAR()] = this.getMDR();
+        }
+        //reset memory
+        fillArray() {
+            for (let i = 0x00; i < this.mainMemory.length; ++i) {
+                this.mainMemory[i] = 0x00;
+            }
+        }
+        //method to check if array has anything besides 0s (is it empty or not)
+        isMemoryEmpty() {
+            for (let i = 0x00; i < this.mainMemory.length; ++i) {
+                if (this.mainMemory[i] != 0x00) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //get total addressable Space --> array is private therefore well use this to get the length
+        totalAddressableSpace() {
+            return this.mainMemory.length + 0x01;
+        }
+        memoryDump(fromAddress, toAddress) {
+            let memoryContents = [];
+            for (let i = fromAddress; i <= toAddress; ++i) {
+                memoryContents.push[this.mainMemory[i]];
+            }
+            return memoryContents;
+        }
+    }
+    TSOS.Memory = Memory;
+})(TSOS || (TSOS = {}));
+//# sourceMappingURL=memory.js.map
---
+++ source/host/memory.ts
@@ -0,0 +1,81 @@
+module TSOS {
+  export class Memory {
+    private mainMemory: number[] = new Array(0xffff); //representation of memory
+    private mar: number = 0x0000; //Memory Address Reg
+    private mdr: number = 0x00; //Memory Data Reg
+
+    constructor() {
+      this.fillArray(); //when Memory is created, it will be initiated to 0
+    }
+
+    //setter for memory address register
+    public setMAR(mar: number): void {
+      this.mar = mar;
+    }
+
+    //getter for memory address register
+    public getMAR(): number {
+      return this.mar;
+    }
+
+    //setter for memory data register
+    public setMDR(mdr: number): void {
+      this.mdr = mdr;
+    }
+
+    //getter for memory data register
+    public getMDR(): number {
+      return this.mdr;
+    }
+
+    //sets mar, mdr, and all of memory to 0
+    public reset(): void {
+      this.fillArray();
+      this.setMAR(0x0000);
+      this.setMDR(0x00);
+    }
+
+    //returns the data (mdr) from a specificed address (mar)
+    public read(): number {
+      return this.mainMemory[this.getMAR()];
+    }
+
+    //sets the data (mdr) from a specificed address (mar)
+    public write(): void {
+      this.mainMemory[this.getMAR()] = this.getMDR();
+    }
+
+    //reset memory
+    public fillArray(): void {
+      for (let i = 0x00; i < this.mainMemory.length; ++i) {
+        this.mainMemory[i] = 0x00;
+      }
+    }
+
+    //method to check if array has anything besides 0s (is it empty or not)
+    public isMemoryEmpty(): boolean {
+      for (let i = 0x00; i < this.mainMemory.length; ++i) {
+        if (this.mainMemory[i] != 0x00) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    //get total addressable Space --> array is private therefore well use this to get the length
+    public totalAddressableSpace(): number {
+      return this.mainMemory.length + 0x01;
+    }
+
+    public memoryDump(
+      fromAddress: number,
+      toAddress: number
+    ): Array<string | number> {
+      let memoryContents = [];
+      for (let i = fromAddress; i <= toAddress; ++i) {
+        memoryContents.push[this.mainMemory[i]];
+      }
+      return memoryContents;
+    }
+  }
+}
