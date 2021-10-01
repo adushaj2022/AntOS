72f97a0833564d699b97e72c8a44d2a5087ea015
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-09-30 21:48:35 -0400

    added table for processes

--- source/os/processControlBlock.ts
+++ source/os/processControlBlock.ts
@@ -8,6 +8,7 @@ module TSOS {
   export class ProcessControlBlock {
     pid: number = 0;
     state: "waiting" | "termintating" | "exiting" | "ready" = "ready";
+    iRegister: number = 0;
     xRegister: number = 0;
     yRegister: number = 0;
     zRegister: number = 0;
