508f463d763136536021bbbc067debd0fa7a20b8
adushaj2022 <anthony.dushaj1@marist.edu>
2021-12-11 11:20:14 -0500

    setup for roll in roll out processes

---
+++ distrib/os/swapper.js
@@ -0,0 +1,13 @@
+var TSOS;
+(function (TSOS) {
+    class Swapper {
+        static roll_in() {
+            // write to disk
+        }
+        static roll_out() {
+            // read from disk and place process on ready queue
+        }
+    }
+    TSOS.Swapper = Swapper;
+})(TSOS || (TSOS = {}));
+//# sourceMappingURL=swapper.js.map
---
+++ source/os/swapper.ts
@@ -0,0 +1,10 @@
+module TSOS {
+  export class Swapper {
+    public static roll_in() {
+      // write to disk
+    }
+    public static roll_out() {
+      // read from disk and place process on ready queue
+    }
+  }
+}
